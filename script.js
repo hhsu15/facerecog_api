795d6e642b20c870f38363ab970c10a9b1cf070f
Hsinheng Hsu <hsin@Hsinhengs-MBP.fios-router.home>
2018-09-04 23:04:02 -0400

    update

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt  = require('bcrypt-nodejs')
 const cors = require('cors')
+const knex = require('knex')
+
+const db = knex ({ 
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'hsin',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+console.log(db.select('*').from('users'))
 
 const app = express()
 //middleware to parse the body becasue express does not know what body is
@@ -54,10 +67,34 @@ app.post('/signin' , (req, res) => {
 
 app.post('/register', (req, res) => {
 	const { email, name, password, age } = req.body
-	
-	bcrypt.hash(password, null, null, function(err, hash) {
-    	console.log(hash)
-    });
+	//create a "transaction" when you have to do two things at once
+	//in here, we have to insert the hash and email into login table
+	//then we insert the records into users table
+	const hash = bcrypt.hashSync(password)
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+			return trx('users')
+			.returning('*') //returns all columns
+			.insert({
+				email: loginEmail[0],
+				name: name,
+				joined: new Date()
+			})
+			.then(user => {
+			res.json(user[0])
+			})
+	    })
+		.then(trx.commit)
+		.catch(trx.rollback)
+	})
+	.catch(err => res.status(400).json('unable to register!'))
+})
 /*
 // Load hash from your password DB.
    bcrypt.compare("bacon", hash, function(err, res) {
@@ -67,46 +104,31 @@ app.post('/register', (req, res) => {
     // res = false
    });
 */
-	console.log(email)
-	database.users.push({
-		id: 12,
-		name: name,
-		email: email,
-	    password: password,
-		age: age
-	})
-    res.json(database.users)
-})
 
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params
-	let found = false
-	database.users.forEach(user => {
-    	if (user.id === id) {
-		found = true
-		return res.json(user)
-	    }
-    })
-	if (!found) {
-		res.status(400).json('not found')
-	}
+	//let found = false
+	db.select('*').from('users').where({id}) //same as {id:id}
+	  .then(user => {
+	  if (user.length) {
+	  	res.json(user[0])
+	  } else {
+	  	res.status(400).json('Not found')
+	  }
+	})
+	.catch(err => res.status(400).json('error getting user'))
 })
 
 app.put('/image', (req, res) => {
 	const { id } = req.body
-	let found = false
-	database.users.forEach(user => {
-    	if (user.id === id) {
-		found = true
-		user.entries++
-		console.log('entries:',user.entries)
-       return res.json(user.entries)
-	   }
+	db('users').where('id','=',id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries =>{
+		res.json(entries[0])
 	})
-	if (!found) {
-		res.status(400).json('not found')
-	}
+	.catch(err => res.status(400).json('failed to add entries'))
 })
 
 app.listen(3000, () => {
